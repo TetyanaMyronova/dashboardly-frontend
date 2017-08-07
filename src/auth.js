b671f28dbbd9ea4cccbc3fb47910711e299b300a
Tetyana Myronova <tetyanamyronova@LWCHAmyronovate.local>
2017-08-07 13:56:43 -0400

    Signup, Logout implemented

--- src/auth.js
+++ src/auth.js
@@ -1,27 +1,38 @@
 import api from './api';
 
 module.exports = {
-  login(email, pass) {
-    if (localStorage.token) {
-      throw new Error('Already logged in')
-    }
-    else {
-      return api.requestLogin(email, pass)
-      .then(res => localStorage.token = res.body.token)
-    }
-  },
 
-  getToken() {
-    return localStorage.token
-  },
+    signup(email, pass) {
 
-  logout() {
-    return api.requestLogout(localStorage.token)
-    .then(res => delete localStorage.token)
-  },
+        if (localStorage.token) {
+            throw new Error('Already logged in') //revise it later
+        }
+        else {
+            return api.requestSignup(email, pass)
+        }
+    },
 
-  isLoggedIn() {
-    return !!localStorage.token
-  },
-  
+    login(email, pass) {
+        if (localStorage.token) {
+            throw new Error('Already logged in')
+        }
+        else {
+            return api.requestLogin(email, pass)
+                .then(res => localStorage.token = res.body.token)
+        }
+    },
+
+    getToken() {
+        return localStorage.token
+    },
+
+    logout() {
+        return api.requestLogout(localStorage.token)
+            .then(res => delete localStorage.token)
+    },
+
+    isLoggedIn() {
+        return !!localStorage.token
+    },
+
 }
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  API_HOST: "https://private-anon-c3f982c0e0-dashboardly.apiary-mock.com"
+  API_HOST: "https://private-aa273-dashboardlyfrontend1.apiary-mock.com"
 }
