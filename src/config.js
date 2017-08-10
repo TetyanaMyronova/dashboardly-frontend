4f742d581184416ec00e6f270e23db1e0c55fd91
TSirrus <tsirrus@gmail.com>
2017-08-10 15:17:56 +0000

    Use constants for server

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+// const ngrok = "https://80c06665.ngrok.io";
+const chhayaApiary = "https://private-aa273-dashboardlyfrontend1.apiary-mock.com";
+
 module.exports = {
-  API_HOST: "https://private-aa273-dashboardlyfrontend1.apiary-mock.com"
-}
+  API_HOST: chhayaApiary
+};
